df3241e959e6bb39c64d59a824d7053d564538b2
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-07-26 14:51:46 +0300

    Update classRecord.js

--- src/modules/classRecord.js
+++ src/modules/classRecord.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 export class Record {
-    constructor(name, score) {
-      this.name = name;
-      this.score = score;
-    }
-  }  
+  constructor(name, score) {
+    this.name = name;
+    this.score = score;
+  }
+}  
