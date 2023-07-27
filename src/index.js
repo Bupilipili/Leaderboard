1ee3e350ba2b14ef4141028c9903943962d592c7
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-07-27 19:41:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-/* eslint-disable no-unused-vars */
-
 import './style.css';
-import { RecordsCollection } from './modules/classCollection.js';
+import  RecordsCollection from './modules/classCollection.js';
 
 // Create an instance of the RecordsCollection class
 const recordsCollection = new RecordsCollection();
+recordsCollection();
