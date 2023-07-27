136ee896d24af93beefc31c0474c25d8db376da5
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-07-27 14:04:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './style.css';
 import { RecordsCollection } from './modules/classCollection.js';
 
+// Create an instance of the RecordsCollection class
 const recordsCollection = new RecordsCollection();
-
-recordsCollection.addRecords();
